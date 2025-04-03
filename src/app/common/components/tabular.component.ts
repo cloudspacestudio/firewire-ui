1899ddfec2949ba302a69f354572874afe87de46
Steven Sederburg <Steven.Sederburg@bigtincan.com>
2025-04-02 23:51:15 -0500

    tabular component enhancements

--- src/app/common/components/tabular.component.ts
+++ src/app/common/components/tabular.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from "@angular/core";
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
 import { NgFor } from "@angular/common";
 
 @Component({
@@ -11,9 +13,7 @@ import { NgFor } from "@angular/common";
             </thead>
             <tbody>
                 <tr *ngFor="let row of datasource">
-                    <td *ngFor="let column of this.getKeys()">
-                        {{this.renderValue(row[column])}}
-                    </td>
+                    <td *ngFor="let column of this.getKeys()" [innerHtml]="this.renderValue(row[column])"></td>
                 </tr>
             </tbody>
         </table>
@@ -23,6 +23,8 @@ import { NgFor } from "@angular/common";
 export class TabularComponent {
     @Input() datasource: any[] = []
 
+    constructor(private sanitizer: DomSanitizer) {}
+
     getKeys() {
         if (!this.datasource || !Array.isArray(this.datasource) || this.datasource.length <= 0) {
             return []
@@ -31,6 +33,31 @@ export class TabularComponent {
     }
 
     renderValue(input: any): any {
+        const images = ['.jpg', 'jpeg', '.gif', '.png','.bmp']
+        if (input && input.toLowerCase && input.toLowerCase().startsWith('http')) {
+            const lastFourChars = input.toLowerCase().substring(input.length - 4)
+            if (images.indexOf(lastFourChars)>=0) {
+                const text = `<a href="${input}" target="_blank">
+                <img src="${input}" style="height: 80px;max-height: 80px;" />
+                </a>`
+                const response: SafeHtml = this.sanitizer.bypassSecurityTrustHtml(text)
+                return response
+            } else if (input.toLowerCase().endsWith('.pdf')) {
+                const text = `<a href="${input}" target="_blank">
+                <img src="/images/pdf.png" style="width: 40px;max-width: 40px;" />
+                </a>`
+                const response: SafeHtml = this.sanitizer.bypassSecurityTrustHtml(text)
+                return response
+            } else if (input.toLowerCase().endsWith('.txt')) {
+                const text = `<a href="${input}" target="_blank">
+                <img src="/images/txt.png" style="width: 40px;max-width: 40px;" />
+                </a>`
+                const response: SafeHtml = this.sanitizer.bypassSecurityTrustHtml(text)
+                return response
+            } else {
+                return `<a href="${input}"  target="_blank">${input}</a>`
+            }
+        }
         return input
     }
 }
